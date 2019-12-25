94efb7acd01af26b14f78f0f148ee0faf3abbbec
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-25 09:32:02 +0530

    mao

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
-// For each
-
- const numbers=[20,21,22,23,24,25,26,27,28,29]
-
-const double=numbers.map(function odd(num){
-	return num*2;
-});
-
-for(i=0;i<double.length;i++)
-console.log(double[i]);
-
-const double2=numbers.map(even);
-
-for(i=0;i<double2.length;i++)
-console.log(double2[i]);
+const words =['asap','byob','rsvp','diy'];
+const words2=words.map(function to(word){
+return word.toUpperCase().split('').join(".");
 
 
- function even(num){
-  if (num%2==0){
-  return true;  }
-else{
-	return false;
-}}
+})
+
