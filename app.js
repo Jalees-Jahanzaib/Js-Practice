e4a272baec4318561ee35e65e0654a096772470f
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-25 09:25:54 +0530

    callbacks 2

--- app.js
+++ app.js
@@ -2,19 +2,22 @@
 
  const numbers=[20,21,22,23,24,25,26,27,28,29]
 
-numbers.forEach(even);
-  
+const double=numbers.map(function odd(num){
+	return num*2;
+});
 
+for(i=0;i<double.length;i++)
+console.log(double[i]);
+
+const double2=numbers.map(even);
+
+for(i=0;i<double2.length;i++)
+console.log(double2[i]);
 
 
  function even(num){
   if (num%2==0){
-  console.log('True')  }
-
-  console.log('False')
-  
-
-
-
-
-}
+  return true;  }
+else{
+	return false;
+}}
