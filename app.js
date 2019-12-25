8d04824cfd8ef2b7a124a0fb07a0f4e71b86f8a0
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-25 09:45:44 +0530

    Arrow functions 2

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
-//Arrow Functions
+// For each
 
-const square = function (x) {
-	return x*x;
-}
-const  life =(x)=>{
-	return x*x;
-}
+ const numbers=[20,21,22,23,24,25,26,27,28,29]
+
+const double=numbers.map(function odd(num){
+	return num*2;
+});
+
+
+
+const double2=numbers.map((n,y)=>{
+	return n*n+ y;
+});
+
+
+
+for(i=0;i<double2.length;i++)
+console.log(double2[i]);
