7e096d92f5ed986ac2fd8881a81821f5f9468dbb
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-25 10:00:10 +0530

    Find 2

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
-let movies = [
-  "The Fantastic Mr. Fox",
-  "Mr. and Mrs. Smith",
-  "Mrs. Doubtfire",
-  "Mr. Deeds"
-]
+// let movies = [
+//   "The Fantastic Mr. Fox",
+//   "Mr. and Mrs. Smith",
+//   "Mrs. Doubtfire",
+//   "Mr. Deeds"
+// ]
 
 
 
-const movie=movies.find(n=>{
-	return n.indexOf("Mrs") ===0;
-})
+// const movie=movies.find(n=>{
+// 	return n.indexOf("Mrs") ===0;
+// })
 
 // Find first movie that includes 'Mrs' in its title:
 // const movie = movies.find(movie => {
@@ -19,31 +19,35 @@ const movie=movies.find(n=>{
 // //Find first movie that begins with 'Mrs':
 // const movie2 = movies.find(m => m.indexOf("Mrs") === 0);
 
-// const books = [{
-//     title: 'Good Omens',
-//     authors: ['Terry Pratchett', 'Neil Gaiman'],
-//     rating: 4.25
-//   },
-//   {
-//     title: 'Bone: The Complete Edition',
-//     authors: ['Jeff Smith'],
-//     rating: 4.42
-//   },
-//   {
-//     title: 'American Gods',
-//     authors: ['Neil Gaiman'],
-//     rating: 4.11
-//   },
-//   {
-//     title: 'A Gentleman in Moscow',
-//     authors: ['Amor Towles'],
-//     rating: 4.36
-//   }
-// ]
-// //Find first book w/ rating over 4.3
+const books = [{
+    title: 'Good Omens',
+    authors: ['Terry Pratchett', 'Neil Gaiman'],
+    rating: 4.25
+  },
+  {
+    title: 'Bone: The Complete Edition',
+    authors: ['Jeff Smith'],
+    rating: 4.42
+  },
+  {
+    title: 'American Gods',
+    authors: ['Neil Gaiman'],
+    rating: 4.11
+  },
+  {
+    title: 'A Gentleman in Moscow',
+    authors: ['Amor Towles'],
+    rating: 4.36
+  }
+]
+//Find first book w/ rating over 4.3
 // const goodBook = books.find(b => b.rating >= 4.3);
 
 // //Find first book with Neil Gaiman as an author:
 // const neilBook = books.find(b => (
 //   b.authors.includes('Neil Gaiman')
 // ))
+
+const goodBook=books.find(n=>{
+	return n.rating>=4.42;
+})
