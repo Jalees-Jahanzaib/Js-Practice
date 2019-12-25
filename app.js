15d9daef3c9f2df305d6a36c35b8da0d0b97973f
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-25 10:55:53 +0530

    Reduce

--- app.js
+++ app.js
@@ -1,40 +1,16 @@
-let movies = [
-  "The Fantastic Mr. Fox",
-  "Mr. and Mrs. Smith",
-  "Mrs. Doubtfire",
-  "Mr. Deeds"
-]
+const nums = [3, 4, 5, 6, 7];
+// To multiply all values in nums:
+const product = nums.reduce((total, currentVal) => {
+  return total * currentVal;
+});
 
-// Find first movie that includes 'Mrs' in its title:
-const movie = movies.find(movie => {
-  return movie.includes('Mrs');
-})
+// total    currentVal    returnVal
+// 3             4           12
+// 12            5           60
+// 60            6           360
+// 360           7           2520
 
-//Find first movie that begins with 'Mrs':
-const movie2 = movies.find(m => m.indexOf("Mrs") === 0);
-
-const books = [{
-    title: 'Good Omens',
-    authors: ['Terry Pratchett', 'Neil Gaiman'],
-    rating: 4.25
-  },
-  {
-    title: 'Bone: The Complete Edition',
-    authors: ['Jeff Smith'],
-    rating: 4.42
-  },
-  {
-    title: 'American Gods',
-    authors: ['Neil Gaiman'],
-    rating: 4.11
-  },
-  {
-    title: 'A Gentleman in Moscow',
-    authors: ['Amor Towles'],
-    rating: 4.36
-  }
-]
-//Find first book w/ rating over 4.3
-const goodBook = books.some(books=>{
-	return books.rating>=4.4;
+//Final Return Value: 2520
+const sum=nums.reduce((final,cueent)=>{
+	return final+cueent;
 })
