c72d8c9fcf0992cb88dbb650af93131015be2afc
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-25 17:44:04 +0530

    Deconstructing arrays

--- app.js
+++ app.js
@@ -1,27 +1,20 @@
-function sayHi() {
-  console.log("HI")
-  //this refers to the window (global scope object in the browser)
-  console.log(this);
+const annoyer= {
+
+pharases:["literally",'crary cray','I cant even ',"YOLO","hc","actually",'aaa']
+,
+pickphrase(){
+	const arr=this.pharases;
+	const idx = Math.floor(Math.random()*arr.length);
+	return arr[idx];
+}
+,
+start(){
+	
+	setInterval(()=>{
+console.log(this.pickphrase())
+
+	},3000);
 }
 
 
-const person = {
-  first: 'Cherilyn',
-  last: 'Sarkisian',
-  nickName: 'Cher',
-  fullName() {
-    //In a method, this refers to the object the method "lives" in:
-    const {
-      first,
-      last,
-      nickName
-    } = this;
-    return `${first} ${last } AKA ${nickName}`;
-  },
-  printBio() {
-    const fullName = this.fullName();
-    console.log(`${fullName} is a person!`)
-  }
 }
-
-// const lala=person.printBio();
