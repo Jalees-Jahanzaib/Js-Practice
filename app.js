4f78d900a19cb789b006318a1e7773f2cdc4a111
Jalees-Jahanzaib <jalees.jahanzaib@students.iiit.ac.in>
2019-12-27 11:09:58 +0530

    DOM attributes

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-// To select all elements with the class of 'special':
-document.getElementsByClassName('special');
-
-//getElementsByClassName also returns an array-like object.
-// We don't have access to array methods, but we can iterate:
-
-const specials = document.getElementsByClassName('special');
-for (let el of specials) {
-  console.log(el);
-}
-
-//We can use spread to make an actual array:
-const arr = [...specials];
-arr.pop() //this works because it's now an array!
+  "
+const range = document.querySelector('input(type="text")')
+VM1408:1 Uncaught DOMException: Failed to execute 'querySelector' on 'Document': 'input(type="text")' is not a valid selector.
+    at <anonymous>:1:24
+(anonymous) @ VM1408:1
+const lol = document.querySelector('input(type="text")')
+VM1419:1 Uncaught DOMException: Failed to execute 'querySelector' on 'Document': 'input(type="text")' is not a valid selector.
+    at <anonymous>:1:22
+(anonymous) @ VM1419:1
+const lol = document.querySelector('input(password="text")')
+VM1424:1 Uncaught SyntaxError: Identifier 'lol' has already been declared
+    at <anonymous>:1:1
+(anonymous) @ VM1424:1
+const lol2 = document.querySelector('input(password="text")')
+VM1430:1 Uncaught DOMException: Failed to execute 'querySelector' on 'Document': 'input(password="text")' is not a valid selector.
+    at <anonymous>:1:23
+(anonymous) @ VM1430:1
+const lol2 = document.querySelector('input[type="password"]')
+VM1454:1 Uncaught SyntaxError: Identifier 'lol2' has already been declared
+    at <anonymous>:1:1
+(anonymous) @ VM1454:1
+const lol3 = document.querySelector('input[type="password"]')
+undefined
+lol3.placeholder="let's Rock"
+"let's Rock"
